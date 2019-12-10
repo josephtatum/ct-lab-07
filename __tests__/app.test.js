d44cec1de3c0e724c116f1e9ad366e8efc1a65d0
Joseph Tatum <josephtatum@gmail.com>
2019-12-10 13:41:17 -0800

    created passing test for delete a recipe

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -46,6 +46,29 @@ describe('app routes', () => {
       });
   });
 
+  it('gets a recipe by id', async () => {
+    const recipe = await Recipe.create({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ],
+    });
+
+    return request(app)
+      .get(`/api/v1/recipes/${recipe._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          '__v': 0,
+          '_id': recipe._id.toString(),
+          'directions': ['preheat oven to 375', 'mix ingredients', 'put dough on cookie sheet', 'bake for 10 minutes'],
+          'name': 'cookies'
+        });
+      });
+  });
+
   it('gets all recipes', async() => {
     const recipes = await Recipe.create([
       { name: 'cookies', directions: [] },
@@ -94,7 +117,7 @@ describe('app routes', () => {
       });
   });
 
-  it('gets a recipe by _id', async() => {
+  it('deletes a recipe by id', async() => {
     const recipe = await Recipe.create({
       name: 'cookies',
       directions: [
@@ -106,14 +129,19 @@ describe('app routes', () => {
     });
 
     return request(app)
-      .get(`/api/v1/recipes/${recipe._id}`)
+      .delete(`/api/v1/recipes/${recipe._id}`)
       .then(res => {
         expect(res.body).toEqual({
-          '__v': 0,
-          '_id': recipe._id.toString(),
-          'directions': ['preheat oven to 375', 'mix ingredients', 'put dough on cookie sheet', 'bake for 10 minutes'],
-          'name': 'cookies' });
+          _id: expect.any(String),
+          name: 'cookies',
+          directions: [
+            'preheat oven to 375',
+            'mix ingredients',
+            'put dough on cookie sheet',
+            'bake for 10 minutes'
+          ],
+          __v: 0
+        });
       });
   });
-
 });
