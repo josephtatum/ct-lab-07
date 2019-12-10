52418f010554fcf4ebedb0fb8bf7a6c6e595edee
Joseph Tatum <josephtatum@gmail.com>
2019-12-10 15:19:38 -0800

    finished writing all passing tests for attempt model

--- __tests__/attempt-app.test.js
+++ __tests__/attempt-app.test.js
@@ -40,7 +40,7 @@ describe('attempt app routes', () => {
       });
   });
 
-  it('gets a recipe by id', async() => {
+  it('gets an attempt by id', async() => {
     const attempt = await Attempt.create({
       recipeId: '123',
       dateOfEvent: '06/28/1991',
@@ -62,99 +62,65 @@ describe('attempt app routes', () => {
       });
   });
 
-  it.skip('gets all recipes', async() => {
-    const recipes = await Recipe.create([
-      { name: 'cookies', directions: [], ingredients: [] },
-      { name: 'cake', directions: [], ingredients: [] },
-      { name: 'pie', directions: [], ingredients: [] }
+  it('gets all attempts', async() => {
+    const attempts = await Attempt.create([
+      { dateOfEvent: '06/28/1991', recipeId: '121' },
+      { dateOfEvent: '06/28/1991', recipeId: '122' },
+      { dateOfEvent: '06/28/1991', recipeId: '123' }
     ]);
 
     return request(app)
-      .get('/api/v1/recipes')
+      .get('/api/v1/attempts')
       .then(res => {
-        recipes.forEach(recipe => {
+        attempts.forEach(attempt => {
           expect(res.body).toContainEqual({
-            _id: recipe._id.toString(),
-            name: recipe.name
+            _id: attempt._id.toString(),
+            name: attempt.name
           });
         });
       });
   });
 
-  it.skip('updates a recipe by id', async() => {
-    const recipe = await Recipe.create({
-      name: 'cookies',
-      directions: [
-        'preheat oven to 375',
-        'mix ingredients',
-        'put dough on cookie sheet',
-        'bake for 10 minutes'
-      ],
-      ingredients: [{
-        amount: '4',
-        name: 'Cardamom',
-        measurement: 'cup'
-      }]
+  it('updates a recipe by id', async() => {
+    const attempt = await Attempt.create({
+      recipeId: '123',
+      dateOfEvent: '06/28/1991',
+      notes: ['I am a note'],
+      rating: 2
     });
 
     return request(app)
-      .patch(`/api/v1/recipes/${recipe._id}`)
-      .send({ name: 'good cookies' })
+      .patch(`/api/v1/attempts/${attempt._id}`)
+      .send({ recipeId: '1234' })
       .then(res => {
         expect(res.body).toEqual({
-          _id: expect.any(String),
-          name: 'good cookies',
-          directions: [
-            'preheat oven to 375',
-            'mix ingredients',
-            'put dough on cookie sheet',
-            'bake for 10 minutes'
-          ],
-          ingredients: [{
-            _id: res.body.ingredients[0]._id,
-            amount: '4',
-            name: 'Cardamom',
-            measurement: 'cup'
-          }],
+          _id: attempt._id.toString(),
+          recipeId: '1234',
+          dateOfEvent: '1991-06-28T07:00:00.000Z',
+          notes: ['I am a note'],
+          rating: 2,
           __v: 0
         });
       });
   });
 
-  it.skip('deletes a recipe by id', async() => {
-    const recipe = await Recipe.create({
-      name: 'cookies',
-      directions: [
-        'preheat oven to 375',
-        'mix ingredients',
-        'put dough on cookie sheet',
-        'bake for 10 minutes'
-      ],
-      ingredients: [{
-        amount: '4',
-        name: 'Cardamom',
-        measurement: 'cup'
-      }]
+  it('deletes a recipe by id', async() => {
+    const attempt = await Attempt.create({
+      recipeId: '123',
+      dateOfEvent: '06/28/1991',
+      notes: ['I am a note'],
+      rating: 2
     });
 
     return request(app)
-      .delete(`/api/v1/recipes/${recipe._id}`)
+      .delete(`/api/v1/attempts/${attempt._id}`)
       .then(res => {
         expect(res.body).toEqual({
-          _id: expect.any(String),
-          name: 'cookies',
-          directions: [
-            'preheat oven to 375',
-            'mix ingredients',
-            'put dough on cookie sheet',
-            'bake for 10 minutes'
-          ],
-          ingredients: [{
-            _id: res.body.ingredients[0]._id,
-            amount: '4',
-            name: 'Cardamom',
-            measurement: 'cup'
-          }],
+          _id: attempt._id.toString(),
+          recipeId: '123',
+          dateOfEvent: '1991-06-28T07:00:00.000Z',
+          notes: ['I am a note'],
+          rating: 2,
           __v: 0
         });
       });
